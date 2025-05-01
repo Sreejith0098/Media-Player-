6939ad5b5ffd1108684c893eeacd0d99c6e3738f
Sreejith R <rsreejith090@gmail.com>
2025-05-01 09:19:20 +0530

    footer completed

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import React from "react";
+import "react-bootstrap";
 
+import { Link } from "react-router-dom";
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <>
+      <div style={{ backgroundColor: "black" }} className=" container-fluid row mt-4 p-4 w-100">
+        <div className="col-lg-5">
+          <h4> <i class="fa-solid fa-music"></i>{" "} Media Player</h4>
+          <p>
+            Designed and build with all the love in the world by the Luminar
+            team with the help of our contributors.
+          </p>
+          <p>Code licenced Luminar, docs CC BY 3.o.</p>
+          <p>Currently v5.3.2.0</p>
+        </div>
+        <div className="col-lg-2">
+          <h4>Links</h4>
+          <Link to={"./"} style={{ textDecoration: "none" }}>Landing Page</Link>
+          <br />
+          <Link to={"./home"} style={{ textDecoration: "none" }}>Home Page</Link>
+          <br />
+          <Link to={"./history"} style={{ textDecoration: "none" }}>History Page </Link>
+        </div>
+        <div className="col-lg-2">
+          <h4>Guides</h4>
+          <p>React</p>
+          <p>React Router</p>
+          <p>React Bootstrap</p>
+        </div>
+        <div className="col-lg-3">
+          <h4>contacts</h4>
+          <input style={{ height: "43px" }} type="text" />{" "}
+          <button
+            style={{
+              backgroundColor: "violet ",
+              height: "30px",
+              paddingBottom: "33px",
+              fontWeight: "800",
+            }}
+            className="btn text-center "
+          >
+            &rarr;
+          </button>
+          <br />
+          <i
+            style={{ fontSize: "19px" }}
+            class="fa-brands fa-twitter  me-2 mt-1 "
+          ></i>
+          <i
+            style={{ fontSize: "19px" }}
+            class="fa-brands fa-instagram me-2 mt-1"
+          ></i>
+          <i
+            style={{ fontSize: "19px" }}
+            class="fa-brands fa-facebook me-2 mt-1"
+          ></i>
+          <i
+            style={{ fontSize: "19px" }}
+            class="fa-brands fa-linkedin me-2 mt-1"
+          ></i>
+          <i
+            style={{ fontSize: "19px" }}
+            class="fa-brands fa-github me-2 mt-1"
+          ></i>
+          <i
+            style={{ fontSize: "19px" }}
+            class="fa-solid fa-phone me-2 mt-1"
+          ></i>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -1,20 +1,105 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
+import musicImg from "../assets/Music-gif.gif";
+import Card from "react-bootstrap/Card";
+import img1 from "../assets/manage-videos.jpg";
+import img2 from "../assets/categorize.jpeg";
+import img3 from "../assets/history.jpeg";
 const Landing = () => {
   return (
-    <div className='container' >
-{/* welcome section  */}
-<div className="row">
-  <div className="col-4">
-    <h3>Welcome to <span className='text-warning'>Media Player</span></h3>
-  </div>
-  <div className="col-2"></div>
-  <div className="col-6"></div>
-</div>
-{/* features section  */}
-{/* info section */}
+    <div className="container">
+      {/* welcome section  */}
+      <div className="row align-items-center ">
+        <div className="col-lg-4">
+          <h3>
+            Welcome to <span className="text-warning">Media Player</span>
+          </h3>
+          <p style={{ textAlign: "justify" }} className=" mt-3">
+            Media Player App will allow user to add or remove their uploaded
+            videos from youTube and also allow them to arrange it in different
+            categories by drag and drop. User can also have the provision to
+            manage their watch history as well. What are you waiting for, let
+            starts exploring our site!!!
+          </p>
+          <Link className="btn btn-primary" to={"/home"}>
+            Get Started
+          </Link>
+        </div>
+        <div className="col-lg-2"></div>
+        <div className="col-lg-6">
+          <img src={musicImg} alt="" />
+        </div>
+      </div>
+      {/* features section  */}
+      <div className="my-3">
+        <h3 className="text-center">Features</h3>
+        <div className="row text-center">
+          <div className="col-lg-4">
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src={img1} />
+              <Card.Body>
+                <Card.Title>Managing Videos</Card.Title>
+                <Card.Text>Users can upload ,view and remove videos</Card.Text>
+              </Card.Body>
+            </Card>
+          </div>
+          <div className="col-lg-4">
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src={img2} />
+              <Card.Body>
+                <Card.Title>categorize Videos </Card.Title>
+                <Card.Text>categorize videos</Card.Text>
+              </Card.Body>
+            </Card>
+          </div>
+          <div className="col-lg-4">
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src={img3} />
+              <Card.Body>
+                <Card.Title>Managing History</Card.Title>
+                <Card.Text>
+                  Users can manage the watch history of all videos
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </div>
+        </div>
+      </div>
+      {/* info section */}
+      <div style={{border:'1px solid '}} className="row rounded mt-4 p-4">
+        <div className="col-lg-4">
+          <h2 className="text-warning text-center">simple,Fast and Powerful</h2>
+          <h5 className="mt-3">
+            Play Everything:{" "}
+            <span  style={{fontSize:'15px', fontWeight:'normal'}} >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas,
+              tempore consequatur? Aliquid, animi? Ipsum harum temporibus
+            </span>
+          </h5>
+          <h5 className="mt-3">
+            Play Everything:{" "}
+            <span  style={{fontSize:'15px', fontWeight:'normal'}} >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas,
+              tempore consequatur? Aliquid, animi? Ipsum harum temporibus 
+            </span>
+          </h5>
+          <h5 className="mt-3">
+            Play Everything:{" "}
+            <span style={{fontSize:'15px', fontWeight:'normal'}} >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas,
+              tempore consequatur? Aliquid, animi? Ipsum harum temporibus
+            </span>
+          </h5>
+        </div>
+        <div className="col-lg-2"></div>
+        <div className="col-lg-6">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/ZdMZ40GSVmc?si=hEmGE7ui5PzyBM1K" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        </div>
+      </div>
+      
+     
     </div>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;
